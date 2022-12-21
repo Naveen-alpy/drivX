30271d4c9309daed659a13a371d035a6c5b9f5e7
naveen.techwyse <naveen.techintl@gmail.com>
2022-12-21 11:12:37 +0530

    some style modification

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -8,13 +8,40 @@ import SectionThree from "./section-3";
 import SectionFour from "./section-4";
 import SectionFive from "./section-5";
 import SectionSix from "./section-6";
+import ParticleBackground from "react-particle-backgrounds";
 
 const HomePage = () => {
   useEffect(() => {
     document.title = "DriverX";
   }, []);
+  const settings = {
+    canvas: {
+      canvasFillSpace: true,
+      width: "100%",
+      height: "100%",
+      useBouncyWalls: false,
+    },
+    particle: {
+      particleCount: 50,
+      color: "#94ecbe",
+      minSize: 2,
+      maxSize: 5,
+    },
+    velocity: {
+      directionAngle: 0,
+      directionAngleVariance: 360,
+      minSpeed: 1,
+      maxSpeed: 3,
+    },
+    opacity: {
+      minOpacity: 0,
+      maxOpacity: 0.5,
+      opacityTransitionTime: 3000,
+    },
+  };
   return (
     <>
+      {/* <ParticleBackground settings={settings} /> */}
       <BannerModule />
       <SectionOne />
       <SectionTwo />
